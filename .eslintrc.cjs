0412dd13fb8f10d775d9395253f6ef8fc19523da
EungBug <eungb14@gmail.com>
2023-09-01 13:50:38 +0900

    ⚙️ Chore: lint 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,15 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules/'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-console': 'warn',
+		"semi": 'error', // 세미콜론이 없으면 에러로 취급한다.
+    "no-unused-vars": "error", // 사용하지않는 변수 error처리 하겠다.
+  }
+};
