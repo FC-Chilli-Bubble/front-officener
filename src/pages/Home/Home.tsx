fb68801158cac757da5eb6fcbc8ed8868037a6b3
EungBug <eungb14@gmail.com>
2023-09-08 00:30:06 +0900

    💄 Design: Button 컴포넌트 기본 작업

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,24 @@
+import Button from "@/components/Common/Button";
 import { styled } from "styled-components";
 
 const Home = () => {
 
   return (
     <StyledLayout>
-
+      <Button title="CTA 버튼" onClick={() => { console.log("click test"); }} />
+      <Button type="primary" title="Primary 버튼" onClick={() => { console.log("click test"); }} />
+      <Button title="비활성화 버튼" disabled onClick={() => { console.log("click test"); }} />
+      <Button title="길이 있는 버튼" width="50%" onClick={() => { console.log("click test"); }} />
     </StyledLayout>
   );
 };
 
 const StyledLayout = styled.div`
-  
+  // test
+  padding-top: 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 export default Home;
