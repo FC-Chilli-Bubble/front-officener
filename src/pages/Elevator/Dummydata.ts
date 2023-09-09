79e42ad2c6712eac5cddd4d5c9279516e15def9c
qw4582 <qw789wlsdud@naver.com>
2023-09-09 16:10:39 +0900

    :sparkles:Feat:더미데이터 추가

---
+++ src/pages/Elevator/Dummydata.ts
@@ -0,0 +1,26 @@
+export const DummyElevators = [
+  {
+    elevatorId: 1, // 엘리베이터 호기
+    floor: 1, // 엘리베이터 위치 층
+    direction: 'up', // or "down"
+    status: 'normal' // ,"repair", "full"
+  },
+  {
+    elevatorId: 2, // 엘리베이터 호기
+    floor: 14, // 엘리베이터 위치 층
+    direction: 'down', // or "up"
+    status: 'full' // or "repair", "normal"
+  },
+  {
+    elevatorId: 3, // 엘리베이터 호기
+    floor: 7, // 엘리베이터 위치 층
+    direction: 'up', // or "down"
+    status: 'normal' // or "repair", "normal"
+  },
+  {
+    elevatorId: 4, // 엘리베이터 호기
+    floor: 13, // 엘리베이터 위치 층
+    direction: 'down', // or "up"
+    status: 'normal' // or "repair", "full"
+  }
+];
