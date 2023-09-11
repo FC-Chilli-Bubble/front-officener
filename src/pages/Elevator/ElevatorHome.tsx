d5bef8402acec2776304749d89b72c739c746490
EungBug <eungb14@gmail.com>
2023-09-11 17:38:15 +0900

    💄 Design: CSS 스타일  가이드

--- src/pages/Elevator/ElevatorHome.tsx
+++ src/pages/Elevator/ElevatorHome.tsx
@@ -10,10 +10,8 @@ const ElevatorHome = () => {
           title="엘리베이터"
         />
         <ElevatorSetting>
-          <TextContainer>
-            <ElevatorTitle>오산 테라타워</ElevatorTitle>
-            <ElevatorNumber>1호기,2호기,3호기,4호기</ElevatorNumber>
-          </TextContainer>
+          <ElevatorTitle>오산 테라타워</ElevatorTitle>
+          {/* <ElevatorNumber>1호기,2호기,3호기,4호기</ElevatorNumber> */}
           <SettingButton>엘리베이터 설정</SettingButton>
         </ElevatorSetting>
         <Elevators></Elevators>
@@ -23,42 +21,32 @@ const ElevatorHome = () => {
 };
 
 const StyledContainer = styled.div`
-  padding: 0 16px;
+  padding: 0 20px;
 `;
 const StyledLayout = styled.div``;
 
 const ElevatorSetting = styled.div`
   background-color: yellow;
-  position: absolute;
-  height: 110px;
-  width: 300px;
-  margin: 10px 10%;
-`;
-
-const TextContainer = styled.div`
+  width: 100%;
+  margin-top: 20px;
   display: flex;
   flex-direction: column;
-  gap: 20px;
   justify-content: center;
+  align-items: center;
 `;
 
+
 const ElevatorTitle = styled.h2`
-  display: flex;
   font-size: 25px;
-  justify-content: center;
-`;
-const ElevatorNumber = styled.span`
-  display: flex;
-  justify-content: center;
+  text-align: center;
 `;
 
 const SettingButton = styled.div`
   background-color: blanchedalmond;
-  position: absolute;
   display: flex;
   width: 220px;
   height: 50px;
-  margin: 70px 12%;
+  margin-top: 30px;
 `;
 
 const Elevators = styled.div`
