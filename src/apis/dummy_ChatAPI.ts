a771cb6f906cdef591480c97294aead33be4ff1d
idey <evxrafter@gmail.com>
2023-09-18 19:08:52 +0900

    :hammer: Refactor: 메세지 더미데이터 추가, 매핑하여 렌더

---
+++ src/apis/dummy_ChatAPI.ts
@@ -0,0 +1,164 @@
+export const messageData = {
+  messages: [
+    {
+      messageId: 1,
+      messageType: 'TALK',
+      content: '저기요',
+      senderId: 1,
+      sendTime: '2023-09-06T17:26:56+09:00'
+    },
+    {
+      messageId: 2,
+      messageType: 'TALK',
+      content: '메뉴 말하세요',
+      senderId: 1,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 3,
+      messageType: 'TALK',
+      content: '알아서할게요',
+      senderId: 2,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 4,
+      messageType: 'REMITTED_ALERT',
+      senderId: 2,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 5,
+      messageType: 'TALK',
+      content: '송금했습니다',
+      senderId: 2,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 6,
+      messageType: 'TALK',
+      content: '확인했습니다',
+      senderId: 1,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 7,
+      messageType: 'TALK',
+      content:
+        'dshfiowehfioewjhiofajfkjdsligjhiroehgiorehgiorjkldjfaldjfoiwejfiojdkjfa;lkdjflakdjfoijaei',
+      senderId: 1,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 8,
+      messageType: 'TALK',
+      content: 'ㅈㅅ잘못눌렀어요',
+      senderId: 1,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 9,
+      messageType: 'TALK',
+      content:
+        'difhwoeifhjalkdsjflksdjfiodsjfoisdjflkdsjklcjsdlkcjlkaifhirdhgioerhgierjofljofkasdlfhdfakjghbdmnx vlsdjhivkndzvjoizfklnsd,c',
+      senderId: 3,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 10,
+      messageType: 'TALK',
+      content: 'ㅈㅅㅈㅅㅈㅅㅈㅅ',
+      senderId: 3,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 11,
+      messageType: 'REMITTED_ALERT',
+      senderId: 3,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 12,
+      messageType: 'TALK',
+      content: '송금했습니다',
+      senderId: 3,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 13,
+      messageType: 'TALK',
+      content: '확인했습니다',
+      senderId: 1,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 14,
+      messageType: 'EXIT_ALERT',
+      senderId: 3,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 15,
+      messageType: 'TALK',
+      content: '안되세요',
+      senderId: 1,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 16,
+      messageType: 'ENTER_ALERT',
+      senderId: 1,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 17,
+      messageType: 'TALK',
+      content: '망러ㅣㅑㅁ노ㅓ랴ㅣㅓ랴ㅏㄴ얼마ㅓㄴ아ㅣ러너라ㅣ어ㅏㅣ러니ㅏ어리ㅏㄴ',
+      senderId: 1,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 18,
+      messageType: 'TALK',
+      content: '맛있겠당',
+      senderId: 3,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    },
+    {
+      messageId: 19,
+      messageType: 'TALK',
+      content: 'ㅇ러먀ㅓ랴덜',
+      senderId: 2,
+      sendTime: '2023-09-06T17:26:59+09:00'
+    }
+  ],
+  members: [
+    {
+      id: 1,
+      name: '김종팔',
+      companyName: '아모레시픽 컴퍼니',
+      profileImage: '김종팔.png',
+      amI: false,
+      isHost: true,
+      hasRemitted: true
+    },
+    {
+      id: 2,
+      name: '김종국',
+      companyName: '아모레시픽 컴퍼니',
+      profileImage: '서울대김종국.png',
+      amI: true,
+      isHost: false,
+      hasRemitted: true
+    },
+    {
+      id: 3,
+      name: '이종진',
+      companyName: '아모레시픽 컴퍼니',
+      profileImage: '떡에미친사람.png',
+      amI: false,
+      isHost: false,
+      hasRemitted: false
+    }
+  ]
+};
--- src/components/ChatRoom/ChatBubble.tsx
+++ src/components/ChatRoom/ChatBubble.tsx
@@ -1,94 +1,96 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
+import { messageData } from '../../apis/dummy_ChatAPI';
+import { isSenderMe } from './ChatFunctions';
+import ChatAlert from './ChatAlert';
+import ChatProfile from './ChatProfile';
 
+type TMessageContent = {
+  messageId: number;
+  messageType: string;
+  content?: string;
+  senderId: number;
+  sendTime: string;
+};
 const ChatBubble = () => {
+  const renderChatStyledBubbles = (messageContent: TMessageContent, index: number) => {
+    //이전의 메세지와 보내는 사람이 같은지 판별
+    const isSameAuthorAsPrevious =
+      index > 0 && messageData.messages[index - 1].senderId === messageContent.senderId;
+    //보내는 사람이 나와 같은지 판별
+    const isAuthorMe = isSenderMe(messageContent.senderId);
+    //이전의 메세지가 같은 타입인지 판별
+    const isTypeSameAsPrevious =
+      index > 0 && messageData.messages[index - 1].messageType === messageContent.messageType;
+    //프로필이 필요한지 판별
+    const isProfileNeed =
+      (!isAuthorMe && !isSameAuthorAsPrevious) || (!isAuthorMe && !isTypeSameAsPrevious);
+
+    const gapSize = isSameAuthorAsPrevious && isTypeSameAsPrevious ? '6px' : '16px';
+
+    return (
+      <StyledStyledWrap
+        key={messageContent.messageId}
+        style={{ marginTop: gapSize }}>
+        {isProfileNeed ? <ChatProfile senderId={messageContent.senderId} /> : null}
+        <StyledBubbles className={isAuthorMe ? 'myBubbles' : 'othersBubbles'}>
+          <p>{messageContent.content}</p>
+        </StyledBubbles>
+      </StyledStyledWrap>
+    );
+  };
 
   return (
-    <Container>
-        <BubblesContainer className="othersBubbles">
-        <ProfileCard>
-                <ProfielImg></ProfielImg>
-                <div>임승이</div>
-            </ProfileCard>
-            <Bubbles className="othersBubbles">
-            안녕하세요
-            </Bubbles>
-        </BubblesContainer>
-        <BubblesContainer className="myBubbles">
-            <Bubbles className="myBubbles">
-            하이
-            </Bubbles>
-            <Bubbles className="myBubbles">
-            메뉴 정하세요
-            </Bubbles>
-        </BubblesContainer>
-        <BubblesContainer className="othersBubbles">
-            <ProfileCard>
-                <ProfielImg></ProfielImg>
-                <div>임승이</div>
-            </ProfileCard>
-            <Bubbles className="othersBubbles">
-            로제 닭강정 세트 간장으로요
-            </Bubbles>
-            <Bubbles className="othersBubbles">
-            토핑 없음
-            </Bubbles>
-        </BubblesContainer>
-    </Container>
+    <StyledContainer>
+      {messageData.messages.map((messageContent, index) => {
+        return messageContent.messageType === 'TALK' ? (
+          renderChatStyledBubbles(messageContent, index)
+        ) : (
+          <ChatAlert
+            senderId={messageContent.senderId}
+            type={messageContent.messageType}
+            keyNum={messageContent.messageId}
+          />
+        );
+      })}
+    </StyledContainer>
   );
 };
 
-const Container = styled.div`
-    display: flex;
-    padding: 16px 12px;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 16px;
+const StyledContainer = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  padding: 0 12px 16px 12px;
+  overflow-y: scroll;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
 `;
-const BubblesContainer = styled.div`
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    justify-content: center;
-    gap: 6px;
-    &.myBubbles {
-        align-items: flex-end;
-    }
-    &.othersBubbles{
-        width: fit-content;
-    }
+const StyledStyledWrap = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
-const Bubbles = styled.div`
-    width: fit-content;
-    padding: 11px 16px;
-    justify-content: center;
-    align-items: center;
-    border-radius: 20px;
-    font-size: 14px;
-    &.myBubbles {
-        align-items: flex-end;
-        color: ${({ theme }) => theme.colors.white};
-        background: ${({ theme }) => theme.colors.ctaColor};
-    }
-    &.othersBubbles {
-        margin-left: 60px;
-        background: ${({ theme }) => theme.colors.grayColor1};
-    }
-`;
-const ProfileCard = styled.div`
-    display: flex;
-    height: 50px;
-    align-items: center;
-    align-content: center;
-    gap: 8px;
-    flex-shrink: 0;
-    flex-wrap: wrap;
-`;
-const  ProfielImg = styled.div`
-    width: 54px;
-    height: 50px;
-    background-image: url(vite.svg);
-    background-position: center;
-    background-repeat: no-repeat;
+const StyledBubbles = styled.div`
+  padding: 11px 16px;
+  border-radius: 20px;
+  font-size: 14px;
+  overflow-wrap: break-word;
+  word-break: break-word;
+  &.myBubbles {
+    max-width: calc(100% - 80px);
+    color: ${({ theme }) => theme.colors.white};
+    background: ${({ theme }) => theme.colors.ctaColor};
+    text-align: right;
+    margin-left: auto;
+  }
+  &.othersBubbles {
+    max-width: calc(100% - 120px);
+    margin-left: 60px;
+    margin-right: auto;
+    background: ${({ theme }) => theme.colors.grayColor1};
+  }
 `;
 
 export default ChatBubble;
