06b73fb0a346140fbfe2ca73af6d1365e24eeb8b
idey <evxrafter@gmail.com>
2023-09-18 19:06:16 +0900

    :sparkles: Feat: 인풋값 받아오기

--- src/components/ChatRoom/ChatInput.tsx
+++ src/components/ChatRoom/ChatInput.tsx
@@ -1,46 +1,73 @@
-
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
+import { useState } from 'react';
+import IconSend from '@/assets/chatRooms/ico_send.svg';
 
 const ChatInput = () => {
+  const [inputValue, setInputValue] = useState('');
+  const SEND_ICON = IconSend;
+
+  const handleChange = e => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    console.log(inputValue);
+    e.preventDefault();
+    //웹소켓 통신 연결
+    setInputValue('');
+  };
+
+  const handleKeyPress = e => {
+    e.key === 'Enter' && handleSubmit(e);
+  };
 
   return (
-
-        <Container>
-          <InputBox placeholder="메시지 보내기" />
-          <SendIco/>
-        </Container>
+    <StyledContainer>
+      <StyledInputBox
+        placeholder="메시지 보내기"
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+        value={inputValue}
+      />
+      <StyledSendIco onClick={handleSubmit}>
+        <img
+          src={SEND_ICON}
+          alt=""
+        />
+      </StyledSendIco>
+    </StyledContainer>
   );
 };
 
-const Container = styled.div`
+const StyledContainer = styled.form`
+  bottom: 0;
   display: flex;
-width: 100%;
-padding: 10px 14px;
-justify-content: center;
-align-items: center;
-gap: 11px;
-background: ${({ theme }) => theme.colors.white};
-box-shadow: 0px -4px 20px 0px rgba(0, 0, 0, 0.05);
+  width: 100%;
+  padding: 10px 14px;
+  justify-content: center;
+  align-items: center;
+  gap: 11px;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px -4px 20px 0px rgba(0, 0, 0, 0.05);
 `;
-const InputBox = styled.input`
-width: 100%;
-border: none;
-height: 40px;
-padding: 11px 24px;
-align-items: center;
-border-radius: 8px;
-background: ${({ theme }) => theme.colors.grayColor1};
-&:focus{
+const StyledInputBox = styled.input`
+  width: 100%;
+  border: none;
+  height: 40px;
+  padding: 11px 24px;
+  align-items: center;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.colors.grayColor1};
+  &:focus {
     outline: none;
-}
-`;  
-const SendIco = styled.div`
-    width: 54px;
-    height: 50px;
-    background-image: url(vite.svg);
-    background-position: center;
-    background-repeat: no-repeat;
-
+  }
+`;
+const StyledSendIco = styled.button`
+  border: none;
+  background-color: transparent;
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
 `;
 
 export default ChatInput;
