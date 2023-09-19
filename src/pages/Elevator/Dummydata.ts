2cce61b74669c760a92f7dc7addd91cb55171646
jinyoungpark231 <qw789wlsdud@naver.com>
2023-09-19 20:44:59 +0900

    :books:Docs: 더미데이터 수정

--- src/pages/Elevator/Dummydata.ts
+++ src/pages/Elevator/Dummydata.ts
@@ -2,31 +2,32 @@ export const DummyElevators: IObjectElevators[] = [
   {
     elevatorId: 1, // 엘리베이터 호기
     floor: 21, // 엘리베이터 위치 층
-    direction: 'up', // or "down" or "up"
+    direction: 'up', // or "down"
     status: 'full' // ,"repair", "full"
   },
   {
     elevatorId: 3, // 엘리베이터 호기
-    direction: 'stop', // or "up" or "stop"
+    floor: null, // 엘리베이터 위치 층
+    direction: null, // or "up"
     status: 'repair' // or "repair", "normal"
   },
   {
     elevatorId: 11, // 엘리베이터 호기
     floor: 7, // 엘리베이터 위치 층
-    direction: 'down', // or "down" or "stop"
+    direction: 'down', // or "down"
     status: 'normal' // or "repair", "normal"
   },
   {
-    elevatorId: 12, // 엘리베이터 호기
-    floor: 12, // 엘리베이터 위치 층
-    direction: 'stop', // or "up" or "stop"
+    elevatorId: 4, // 엘리베이터 호기
+    floor: 13, // 엘리베이터 위치 층
+    direction: 'stop', // or "up"
     status: 'normal' // or "repair", "full"
   }
 ];
 
 interface IObjectElevators {
   elevatorId: number;
-  floor?: number;
-  direction: 'stop' | 'up' | 'down';
+  floor: number | null;
+  direction: 'stop' | 'up' | 'down' | null;
   status: 'normal' | 'repair' | 'full';
 }
