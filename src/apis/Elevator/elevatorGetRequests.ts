891f43af4e187338a86f8aaa10c5036cd0fd7dfc
jinyoungpark231 <qw789wlsdud@naver.com>
2023-09-28 19:15:53 +0900

    :memo:Test: 테스트용 토큰 삭제

--- src/apis/Elevator/elevatorGetRequests.ts
+++ src/apis/Elevator/elevatorGetRequests.ts
@@ -1,16 +1,8 @@
 import { apiClient } from '@/apis/apiClient';
 import { ICommonResponse } from '@/types/Common/ICommonResponse';
-import { IObjectElevator } from '@/types/Common/Elevator/Ielevator';
-
-const token =
-  'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJxdzc4OUBuYXZlci5jb20iLCJpYXQiOjE2OTU4MTA4MDAsImV4cCI6MTY5NTgxNDQwMH0.QnbJmsVYtgphAEUfaHnBEPle7NyrKRkw9d-r4JN0cmE';
+import { IObjectElevator } from '@/types/Common/Elevator/IElevator';
 
 export const fetchElevatorObj = async (): Promise<ICommonResponse<IObjectElevator[]>> => {
-  const response = await apiClient.get('/api/elevator', {
-    // 테스트용
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
+  const response = await apiClient.get('/api/elevator');
   return response.data;
 };
