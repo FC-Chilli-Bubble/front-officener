74cc8119dd74e872586988c501e73ff0ab931e14
doitidey <evxrafter@gmail.com>
2023-10-04 01:07:08 +0900

    :hammer: Refactor: api 코드 수정

--- src/apis/ChatRoom/ChatExitApis.tsx
+++ src/apis/ChatRoom/ChatExitApis.tsx
@@ -1,37 +1,17 @@
 import { apiClient } from '@/apis/apiClient';
 import { IKick, IKickRequest } from '@/types/Chatroom/IExit';
 
-const handleApiError = (error: any) => {
-  console.error('API Error:', error);
-  throw error;
-};
-
 //채팅 나가기 post
 export const createExitPost = async (roomId: number) => {
-  try {
-    const response = await apiClient.post(`/api/chat/${roomId}/exit`);
-    return response.data;
-  } catch (error) {
-    handleApiError(error);
-  }
+   await apiClient.post(`/api/chat/${roomId}/exit`);
 };
 
 //나가기 요청 post
 export const createKickRequestPost = async (roomId: number, body : IKickRequest) => {
-  try {
-    const response = await apiClient.post(`/api/chat/${roomId}/kickRequest`, body);
-    return response.data;
-  } catch (error) {
-    handleApiError(error);
-  }
+   await apiClient.post(`/api/chat/${roomId}/kickRequest`, body);
 };
 
 //강퇴 post
 export const createKickPost = async (roomId: number, body : IKick) => {
-  try {
-    const response = await apiClient.post(`/api/chat/${roomId}/kick`, body);
-    return response.data;
-  } catch (error) {
-    handleApiError(error);
-  }
+  await apiClient.post(`/api/chat/${roomId}/kick`, body);
 };
--- src/apis/ChatRoom/ChatHeaderButtonApis.ts
+++ src/apis/ChatRoom/ChatHeaderButtonApis.ts
@@ -1,45 +1,20 @@
 import { apiClient } from '@/apis/apiClient';
 
-const handleApiError = (error: any) => {
-  console.error('API Error:', error);
-  throw error;
-};
-
 // guset 송금 완료
 export const createGuestRemittedPost = async (roomId: number) => {
-  try {
-    const response = await apiClient.post(`/api/chat/${roomId}/remitted`);
-    return response.data;
-  } catch (error) {
-    handleApiError(error);
-  }
+  await apiClient.post(`/api/chat/${roomId}/remitted`);
 };
 
 // guset 수령 완료
 export const createGuestReceivedPost = async (roomId: number) => {
-  try {
-    const response = await apiClient.post(`/api/chat/${roomId}/received`);
-    return response.data;
-  } catch (error) {
-    handleApiError(error);
-  }
+  await apiClient.post(`/api/chat/${roomId}/received`);
 };
 
 //host 참여 마감하기
 export const createHostClosedPost = async (roomId: number) => {
-  try {
-    const response = await apiClient.post(`/api/chat/${roomId}/closed`);
-    return response.data;
-  } catch (error) {
-    handleApiError(error);
-  }
+  await apiClient.post(`/api/chat/${roomId}/closed`);
 };
 //host 배달 완료
 export const createHostDeliveredPost = async (roomId: number) => {
-  try {
-    const response = await apiClient.post(`/api/chat/${roomId}/delivered`);
-    return response.data;
-  } catch (error) {
-    handleApiError(error);
-  }
+  await apiClient.post(`/api/chat/${roomId}/delivered`);
 };
