d887be29fafc967a959fd0b7b32823ac55abec5a
EungBug <eungb14@gmail.com>
2023-10-05 01:45:18 +0900

    🔨 Refactor: 채팅방 아이템 리팩토링

--- src/components/Delivery/ChatItem.tsx
+++ src/components/Delivery/ChatItem.tsx
@@ -1,38 +1,46 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+import { IChat } from '@/types/Delivery/IDeliveryChat';
+import { FOOD_IMAGE } from '@/constants/commonUiData';
 
 interface IChatData {
-  profileImage: string;
-  name: string;
-  message: string;
+  chatItem: IChat;
 }
 
-const ChatItem: React.FC<IChatData> = ({ profileImage, name, message }) => {
+const ChatItem = React.memo(({ chatItem }: IChatData) => {
   const navigate = useNavigate();
 
   const handleMoveChat = () => {
-    navigate('/chatroom');
+    navigate(`/chat/${chatItem.chatRoomId}`);
   };
 
   return (
     <>
       <StyledChatItem onClick={handleMoveChat} >
         <img
-          src={profileImage}
+          src={FOOD_IMAGE[chatItem.foodTag]}
           alt="Profile"
         />
         <div>
-          <strong>{name}</strong>
-          <p>{message}</p>
+          <strong>{chatItem.storeName}</strong>
+          <p>{chatItem.recentMessage}</p>
         </div>
+        {
+          chatItem.numUnreadMessages > 0 &&
+          <StyledChatNum>
+            {chatItem.numUnreadMessages}
+          </StyledChatNum>
+        }
       </StyledChatItem>
       <StyledDivider />
     </>
   );
-};
+});
 
 const StyledChatItem = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
   gap: 15px;
@@ -60,6 +68,21 @@ const StyledDivider = styled.div`
   width: 100%;
   height: 2px;
   background-color: ${props => props.theme.colors.grayColor1};
+`;
+
+const StyledChatNum = styled.div`
+  width: 25px;
+  height: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.colors.redColor0};
+  color: ${props => props.theme.colors.white};
+  border-radius: 50%;
+  position: absolute;
+  right: 12px;
+  font-size: 12px;
+  font-weight: 700;
 `;
 
 export default ChatItem;
